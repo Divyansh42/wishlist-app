efeb7ae5da23e847dc8003f22c712b6816d8ad7f
Divyansh42 <diagrawa@redhat.com>
2020-09-11 16:46:00 +0530

    lesson 2 completed

--- src/models/WishList.test.ts
+++ src/models/WishList.test.ts
@@ -1,4 +1,5 @@
-import { WishListItem } from "./WishList"
+import { WishList, WishListItem } from "./WishList"
+import { isTemplateElement } from "@babel/types";
 it("can create an instance of model", () => {
     const item = WishListItem.create({
         "name" : "Headphone",
@@ -6,4 +7,35 @@ it("can create an instance of model", () => {
         "image" : "https://www.amazon.in/pTron-Bassbuds-Wireless-Bluetooth-Headphones/dp/B07WSHWNH8?ref_=Oct_DLandingS_D_d157f3d0_60&smid=A14CZOWI0VEHLG"
     });
     expect(item.price).toBe(999);
+    item.changeName("Wireless Earphone")
+    expect(item.name).toBe("Wireless Earphone")
 });
+
+it("can create a wishlist", () => {
+    const list = WishList.create({
+        items: [
+
+            {
+                "name" : "Headphone",
+                "price" : 999
+            }
+        ]
+    })
+
+    expect(list.items.length).toBe(1)
+    expect(list.items[0].price).toBe(999)
+})
+
+it("can add new items", () => {
+    const list = WishList.create()
+    list.add(WishListItem.create({
+        name: "Cheese",
+        price: 100
+    }))
+
+    expect(list.items.length).toBe(1);
+    expect(list.items[0].name).toBe("Cheese")
+    list.items[0].changeName("Pizza Cheese")
+    expect(list.items[0].name).toBe("Pizza Cheese")
+    
+})
