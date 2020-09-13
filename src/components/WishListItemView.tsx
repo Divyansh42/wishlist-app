5315cde7800c00b8c8d23b972946c499e3346b9c
Divyansh42 <diagrawa@redhat.com>
2020-09-13 19:50:19 +0530

    Lesson 6: mst models

--- src/components/WishListItemView.tsx
+++ src/components/WishListItemView.tsx
@@ -1,23 +1,63 @@
 import React, { Component } from "react";
+import { observer } from "mobx-react";
+import { clone, getSnapshot, applySnapshot } from "mobx-state-tree";
+
+import WishListItemEdit from "./WishListItemEdit"
 
 // import IWishListItem from "../models/WishList"
 
 class WishListItemView extends Component {
     props: any;
+    state: any;
     constructor(props: any) {
         super(props);
         this.props = props;
+        this.state = { isEditing: false }
     }
     render(): any {
-        const {item } = this.props;
-        return (
+        const { item } = this.props;
+        return this.state.isEditing ? (
+            this.renderEditable()
+        ) : (    
             <li className="item">
                 {item.image && <img src={item.image} />}
                 <h3>{item.name}</h3>
                 <span>{item.price}</span>
+                <span>
+                    <button onClick={this.onToggleEdit}>✏️</button>
+                </span>
             </li>
-        );     
+        ); 
     }
+    renderEditable() {
+        return (
+            <li className="item">
+                <WishListItemEdit item={this.state.clone} />
+                <button onClick={this.onSaveEdit}>💾</button>
+                <button onClick={this.onCancelEdit}>❌</button>
+            </li>
+        )
+    }
+    onToggleEdit = () => {
+        //clone to create a full, deep clone of any model instance
+        this.setState({ 
+            isEditing: true,
+            clone: clone(this.props.item) });
+    };
+
+    onSaveEdit = () => {
+        applySnapshot(this.props.item, getSnapshot(this.state.clone))
+        this.setState({
+            isEditing: false,
+            clone: null
+        })
+    }
+    
+    onCancelEdit = () => {
+        this.setState({
+            isEditing: false
+        })
+    };
 }
 
 // const WishListItemView = ({ item }) => (
@@ -28,4 +68,4 @@ class WishListItemView extends Component {
 //     </li>
 // )
 
-export default WishListItemView
+export default observer(WishListItemView)
